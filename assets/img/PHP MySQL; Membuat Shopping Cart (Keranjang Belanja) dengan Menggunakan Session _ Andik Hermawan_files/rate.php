aaf4a6cb15ce417ec7eaddffb7e17489f94fdfbe
windows <windows@DESKTOP-D8BI0CS>
2018-05-04 17:12:24 +0700

    ini adalah file assets.

---
+++ assets/img/PHP MySQL; Membuat Shopping Cart (Keranjang Belanja) dengan Menggunakan Session _ Andik Hermawan_files/rate.php
@@ -0,0 +1,6 @@
+PDRTJS_1923454_post_433.avg_rating = 4.5;
+PDRTJS_1923454_post_433.votes = 9;		
+PDRTJS_settings_1923454_post_433= {			"type" : "stars",			"size" : "sml",			"star_color" : "yellow",			"custom_star" : "",			"font_size" : "",			"font_line_height" : "16px",			"font_color" : "",			"font_align" : "left",			"font_position" : "right",			"font_family" : "",			"font_bold" : "normal",			"font_italic" : "normal",			"text_votes" : "Votes",			"text_rate_this" : "Rate This",			"text_1_star" : "Very Poor",			"text_2_star" : "Poor",			"text_3_star" : "Average",			"text_4_star" : "Good",			"text_5_star" : "Excellent",			"text_thank_you" : "Thank You",			"text_rate_up" : "Rate Up",			"text_rate_down" : "Rate Down"		};
+PDRTJS_1923454_post_433.init();		
+PDRTJS_1923454_post_433.token='c95827224a9e7d831aa2a64db507bab7';
+/*1923454,_post_433,wp-post-433,608998437,4.5-9*/

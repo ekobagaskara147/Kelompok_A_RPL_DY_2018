39117084ad0fb7a7fdc59e919532c72bd220ed9f
Mariza <Asus@DESKTOP-B9HJINT>
2018-05-11 10:32:59 +0700

    ini controller untuk memanggil view

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -18,4 +18,25 @@ class Web extends CI_Controller {
 	public function menu(){
 		$this->load->view('menu');
 	}
+
+	public function login(){
+		$this->load->view('login');
+	}
+
+	public function login_post(){
+		$no_meja = $this->input->post('no_meja');
+		
+		$this->load->model('Web_model'); // memuat kelas Web_model.php agat fungsi didalamnya bisa dipanggil di controller ini_get
+				
+		$data = array(
+			"no_meja" => $no_meja
+		);
+		//echo $this->Web_model->check_login($data);
+		$user_login = $this->Web_model->check_login($data);
+		if ($user_login){
+			$this->load->view('dashboardpelanggan');
+		} else {
+			echo "Meja tidak tersedia!";
+		}
+	}
 }

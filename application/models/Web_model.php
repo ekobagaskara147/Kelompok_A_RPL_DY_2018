68c25685b3481898f063175eff228c179e0073c0
Asus <Asus@DESKTOP-B9HJINT>
2018-05-11 10:34:21 +0700

    ini model untuk mengecek database login

---
+++ application/models/Web_model.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Web_model extends CI_Model {
+	
+	function __construct()
+    {
+        parent::__construct();
+    }
+
+	/**
+	 * ini adalah fungsi untuk mengecek username dan password
+	 * pada tabel 'user'$this->load->view('menu', $data);
+	 * @param $data -> adalah array yang berisi data username dan password
+	 * return $user, fungsi ini akan mengembalikan jumlah record yang terdapat di table user berdasarkan array data
+	 */
+	public function check_login($data){
+		$qr = $this->db->get_where('pelanggan', $data)->result(); // get_where adalah fungsi pada CI untuk mengambil data bedasarkan informasi tertentu
+		if (count($qr) > 0){
+			return $data;
+		} else {
+			return false;
+		}
+	}
+}

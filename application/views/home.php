73e062409ba5f1c77c731ae3914cc45fdee75936
Unknown <dahlia98@mhs.unsyiah.ac.id>
2018-05-31 10:37:40 +0700

    rapiin

--- application/views/home.php
+++ application/views/home.php
@@ -1,50 +1,51 @@
 <!DOCTYPE html>
+
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
-    <title>Order Now: Sekarang pesan di tempat jadi lebih mudah!</title>
 
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="<?php echo base_url('assets/');?>img/favicon.ico" type="image/x-icon">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">    
+		
+			<title>Order Now: Sekarang pesan di tempat jadi lebih mudah!</title>
 
-    <!-- Font awesome -->
-    <link href="<?php echo base_url('assets/');?>css/font-awesome.css" rel="stylesheet">
-    <!-- Bootstrap -->
-    <link href="<?php echo base_url('assets/');?>css/bootstrap.css" rel="stylesheet">   
-    <!-- Slick slider -->
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/');?>css/slick.css">    
-    <!-- Date Picker -->
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/');?>css/bootstrap-datepicker.css">   
-     <!-- Gallery Lightbox -->
-    <link href="<?php echo base_url('assets/');?>css/magnific-popup.css" rel="stylesheet"> 
-    <!-- Theme color -->
-    <link id="switcher" href="<?php echo base_url('assets/');?>css/theme-color/default-theme.css" rel="stylesheet">     
+				<!-- Favicon -->
+				<link rel="shortcut icon" href="<?php echo base_url('assets/');?>img/favicon.ico" type="image/x-icon">
 
-    <!-- Main style sheet -->
-    <link href="<?php echo base_url('assets/');?>css/style.css" rel="stylesheet">    
+				<!-- Font awesome -->
+				<link href="<?php echo base_url('assets/');?>css/font-awesome.css" rel="stylesheet">
+				<!-- Bootstrap -->
+				<link href="<?php echo base_url('assets/');?>css/bootstrap.css" rel="stylesheet">   
+				<!-- Slick slider -->
+				<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/');?>css/slick.css">    
+				<!-- Date Picker -->
+				<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/');?>css/bootstrap-datepicker.css">   
+				 <!-- Gallery Lightbox -->
+				<link href="<?php echo base_url('assets/');?>css/magnific-popup.css" rel="stylesheet"> 
+				<!-- Theme color -->
+				<link id="switcher" href="<?php echo base_url('assets/');?>css/theme-color/default-theme.css" rel="stylesheet">     
 
-   
-    <!-- Google Fonts -->
+				<!-- Main style sheet -->
+				<link href="<?php echo base_url('assets/');?>css/style.css" rel="stylesheet">    
 
-    <!-- Prata for body  -->
-    <link href='https://fonts.googleapis.com/css?family=Prata' rel='stylesheet' type='text/css'>
-    <!-- Tangerine for small title -->
-    <link href='https://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>   
-    <!-- Open Sans for title -->
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    
-    
+				<!-- Google Fonts -->
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+				<!-- Prata for body  -->
+				<link href='https://fonts.googleapis.com/css?family=Prata' rel='stylesheet' type='text/css'>
+				<!-- Tangerine for small title -->
+				<link href='https://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>   
+				<!-- Open Sans for title -->
+				<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+				<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+				<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+				<!--[if lt IE 9]>
+				  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+				  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+				<![endif]-->
 
   </head>
+  
   <body>
 
   <!--START SCROLL TOP BUTTON -->
@@ -82,6 +83,7 @@
             <li><a href="#mu-about-us">ABOUT US</a></li>                                                                     
             <li><a href="#mu-gallery">GALLERY</a></li>
             <li><a href="#mu-contact">CONTACT</a></li> 
+			<li><a href="<?php echo base_url('index.php/web/login');?>">LOGIN</a></li>
           </ul>                            
         </div><!--/.nav-collapse -->       
       </div>          

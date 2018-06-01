e913f5549feec2d97c2c414581b4333a584caab3
Unknown <unknown@example.com>
2018-06-01 14:18:02 +0700

    getDataPesanan di Dashboard_model

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -12,3 +12,9 @@ class Dashboard_model extends CI_Model {
 		$qr = $this->db->select("*")->get('meja'); // get_where adalah fungsi pada CI untuk mengambil data bedasarkan informasi tertentu
 		return $qr;
 	}
+	
+	public function getDataPesanan($no_meja){
+		$qr = $this->db->query("SELECT * FROM meja INNER JOIN menu_pesanan ON meja.id_pemesanan = menu_pesanan.id_pemesanan INNER JOIN menu ON menu_pesanan.id_item = menu.id_menu where meja.no_meja = '$no_meja'")->result();
+		return $qr;
+		
+	}

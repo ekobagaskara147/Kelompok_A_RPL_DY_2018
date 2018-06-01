8288a5afe0e390daf5c1439693e6de7f7759cd98
Unknown <mariza@mhs.unsyiah.ac.id>
2018-06-01 11:56:28 +0700

    tadi salah tarok alert

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -149,6 +149,7 @@ class Dashboard extends CI_Controller {
         );
 		$this->load->model('Menu_model');
         $this->Menu_model->editMenu($data,$id_menu);
+        $this->session->set_flashdata('notif','<div class="alert alert-success" role="alert"> Data Berhasil ditambahkan <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
         redirect('dashboard/'.$redir_view);
     }
 
@@ -159,7 +160,6 @@ class Dashboard extends CI_Controller {
 		);
 		$this->load->model ('Menu_model');
 		$this->Menu_model->hapusMenu($data);
-		$this->session->set_flashdata('notif','<div class="alert alert-success" role="alert"> Data Berhasil ditambahkan <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
 		redirect('dashboard/'.$redir_view);
 	}
 

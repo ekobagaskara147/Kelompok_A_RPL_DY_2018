d767bc8076d0bcf36a15c44f9691c55a2d082ee7
Unknown <unknown@example.com>
2018-06-01 12:35:28 +0700

    bagian calendar di view dashboard admin

--- application/views/dashboard_kar/dash.php
+++ application/views/dashboard_kar/dash.php
@@ -184,3 +184,34 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		</div>	
 		<!-- //market-->
 	<!--//agileinfo-grap-->
+	
+				</div>
+			</div>
+		</div>
+		<div class="agil-info-calendar">
+		<!-- calendar -->
+		<div class="col-md-6 agile-calendar">
+			<div class="calendar-widget">
+                <div class="panel-heading ui-sortable-handle">
+					<span class="panel-icon">
+                      <i class="fa fa-calendar-o"></i>
+                    </span>
+                    <span class="panel-title"> Calendar Widget</span>
+                </div>
+				<!-- grids -->
+					<div class="agile-calendar-grid">
+						<div class="page">
+							
+							<div class="w3l-calendar-left">
+								<div class="calendar-heading">
+									
+								</div>
+								<div class="monthly" id="mycalendar"></div>
+							</div>
+							
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+			</div>
+		</div>
+		<!-- //calendar -->

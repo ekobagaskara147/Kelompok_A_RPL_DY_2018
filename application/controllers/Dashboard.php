6bbe4e8b41d612e6610a9fae5ea0400f6d53174b
Unknown <mariza@mhs.unsyiah.ac.id>
2018-06-01 11:55:23 +0700

    buat alert kalau udah tambah atau edit makanan

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -135,6 +135,7 @@ class Dashboard extends CI_Controller {
         );
 		$this->load->model('Menu_model');
         $this->Menu_model->addMenu($data);
+        $this->session->set_flashdata('notif','<div class="alert alert-success" role="alert"> Data Berhasil ditambahkan <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
         redirect('dashboard/'.$redir_view);
     }
 
@@ -158,6 +159,7 @@ class Dashboard extends CI_Controller {
 		);
 		$this->load->model ('Menu_model');
 		$this->Menu_model->hapusMenu($data);
+		$this->session->set_flashdata('notif','<div class="alert alert-success" role="alert"> Data Berhasil ditambahkan <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
 		redirect('dashboard/'.$redir_view);
 	}
 

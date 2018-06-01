89641f3e7eb81bb3ae0179da71613335b68ae3d7
Unknown <mariza@mhs.unsyiah.ac.id>
2018-06-01 11:57:48 +0700

    alert data diubah

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -149,7 +149,7 @@ class Dashboard extends CI_Controller {
         );
 		$this->load->model('Menu_model');
         $this->Menu_model->editMenu($data,$id_menu);
-        $this->session->set_flashdata('notif','<div class="alert alert-success" role="alert"> Data Berhasil ditambahkan <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+        $this->session->set_flashdata('notif','<div class="alert alert-success" role="alert"> Data Berhasil diubah <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
         redirect('dashboard/'.$redir_view);
     }
 

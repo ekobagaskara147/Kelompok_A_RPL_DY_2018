a16a80f682218d46084da8dffede6a12d09f7b27
Unknown <unknown@example.com>
2018-06-01 12:42:50 +0700

    script calendar

--- application/views/dashboard_kar/dash.php
+++ application/views/dashboard_kar/dash.php
@@ -303,3 +303,37 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	   
 	});
 	</script>
+<!-- calendar -->
+	<script type="text/javascript" src="<?php echo base_url ('assets2/'); ?>js/monthly.js"></script>
+	<script type="text/javascript">
+		$(window).load( function() {
+
+			$('#mycalendar').monthly({
+				mode: 'event',
+				
+			});
+
+			$('#mycalendar2').monthly({
+				mode: 'picker',
+				target: '#mytarget',
+				setWidth: '250px',
+				startHidden: true,
+				showTrigger: '#mytarget',
+				stylePast: true,
+				disablePast: true
+			});
+
+		switch(window.location.protocol) {
+		case 'http:':
+		case 'https:':
+		// running on a server, should be good.
+		break;
+		case 'file:':
+		alert('Just a heads-up, events will not work when run locally.');
+		}
+
+		});
+	</script>
+	<!-- //calendar -->
+</body>
+</html>

b6155bd3bc0f3c368099cd683d11e90721d8e760
Unknown <mariza@mhs.unsyiah.ac.id>
2018-06-01 11:29:29 +0700

    fungsi index

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -18,4 +18,9 @@ class Dashboard extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	public function index()
+	{
+		$this->load->view('dashboard_kar/dash');
+	}
 }

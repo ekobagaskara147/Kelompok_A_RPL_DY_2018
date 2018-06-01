7287045e9b7115c06642f2a262739294554d8c81
Unknown <eko98@mhs.unsyiah.ac.id>
2018-06-01 13:34:36 +0700

    js untuk dashboard

---
+++ assets2/js/dashboard.js
@@ -0,0 +1,19 @@
+function refreshStatusMeja(){
+	$.ajax({
+		url:"getStatusMeja",
+		success:function(data) {
+			var meja=JSON.parse(data);
+			for(var i=0; i<meja.length; i++){
+				var no_meja = parseInt(meja[i]["no_meja"]);
+				var statusmeja = parseInt(meja[i]["status"]);
+				var selmeja=$("#meja-"+no_meja);
+				if(statusmeja != 0){
+					if (!selmeja.hasClass("reservedtable")){
+						selmeja.addClass("reservedtable");
+					}
+				}
+			}
+		}
+	});
+	setTimeout(refreshStatusMeja, 1000);
+}
